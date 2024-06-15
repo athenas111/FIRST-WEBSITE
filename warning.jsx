a1451e0e964196fba7a33285370c3f81965c6e4d
athenas111 <146963873+athenas111@users.noreply.github.com>
2024-06-15 17:07:08 -0300

    warning.jsx

---
+++ warning.jsx
@@ -0,0 +1,13 @@
+ import './Warning.css'
+const Warning = () => {
+  return (
+    <div>
+         <section className="announcement-banner" tabIndex="0">
+        NEW SINGLE OUT NEW SINGLE OUT NEW SINGLE OUT NEW SINGLE OUT
+        NEW SINGLE OUT NEW SINGLE OUT NEW SINGLE OUT NEW SINGLE OUT  NEW SINGLE OUT NEW SINGLE OUT NEW SINGLE OUT NEW SINGLE OUT  NEW SINGLE OUT NEW SINGLE OUT NEW SINGLE            
+      </section>
+    </div>
+  )
+}
+
+export default Warning
